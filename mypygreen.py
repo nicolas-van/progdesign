508169a4d0f6e2f3f3295ec35ad23bd340c3412e
Nicolas Vanhoren <nicolas.vanhoren.01@gmail.com>
2013-11-14 09:58:06 +0100

    Adapted to work with pygreen 2.0.0

--- mypygreen.py
+++ mypygreen.py
@@ -4,7 +4,7 @@ import os
 import os.path
 import subprocess
 import re
-from bottle import response
+import flask
 
 def markdown(*args, **kw):
     from markdown import markdown
@@ -67,8 +67,8 @@ if __name__ == "__main__":
             less_path = "%s.less" % ".".join(file_path.split(".")[0:-1])
             if not os.path.exists(file_path) and os.path.exists(less_path):
                 css = subprocess.check_output(["lessc", less_path])
-                response.content_type = 'text/css'
-                return css
+                response = flask.Response(css, mimetype='text/css')
+                return response
         return old_file_renderer(path)
     pygreen.file_renderer = file_renderer
 
